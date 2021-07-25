86651049f8a66cef82e44614074fc53ac30a887f
zhangyunan8 <87951815+zhangyunan8@users.noreply.github.com>
2021-07-25 23:11:06 +0100

    Update Person.java

--- src/com/proquest/interview/phonebook/Person.java
+++ src/com/proquest/interview/phonebook/Person.java
@@ -1,7 +1,42 @@
 package com.proquest.interview.phonebook;
 
+/*
+ Because of personal data, feel it is necessary to put some access control in place:
+ 	1. class variables can only be accessed within the declared class itself.
+  	2. class methods, constructor can be only accessed within the same package and subclass within other package
+ */
+
+
 public class Person {
-	public String name;
-	public String phoneNumber;
-	public String address;
+
+	private String name;
+	private String phoneNumber;
+	private String address;
+
+	protected Person(){};
+
+	protected Person (String inputName, String inputPhonenumber, String inputAddress){
+		setName(inputName);
+		setAddress(inputAddress);
+		setPhoneNumber(inputPhonenumber);
+	}
+	// setters and getters
+	protected void setName(String inputName){
+		this.name = inputName;
+	}
+	protected void setPhoneNumber(String inputPhonenumber){
+		this.phoneNumber = inputPhonenumber;
+	}
+	protected void setAddress(String inputAddress){
+		this.address = inputAddress;
+	}
+	protected String getName(){
+		return this.name;
+	}
+	protected String getPhoneNumber(){
+		return this.phoneNumber;
+	}
+	protected String getAddress(){
+		return this.address;
+	}
 }
